1dd027a79034ca47d58d1e9cbd819d4f04344d36
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-07 14:04:03 -0400

    added logging

--- run.py
+++ run.py
@@ -2,64 +2,64 @@
 
 import os
 import sys
-import time
 import json
-import subprocess
+import shutil
+import zipfile
 from datetime import datetime
+import time
+import logging
+from logging.handlers import RotatingFileHandler
 
-CONFIG_FILE = 'config.json'
-EXECUTED_TASKS_FILE = 'executed_tasks.json'
+# Configure logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler("scheduled_task_runner.log", maxBytes=10*1024*1024, backupCount=3)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def load_config():
-    with open(CONFIG_FILE, 'r') as f:
-        config = json.load(f)
-    return config
+    with open('config.json', 'r') as f:
+        return json.load(f)
 
-def save_executed_tasks(executed_tasks):
-    with open(EXECUTED_TASKS_FILE, 'w') as f:
-        json.dump(executed_tasks, f)
+def copy_directory(src, dest):
+    try:
+        shutil.copytree(src, dest)
+        logger.info(f"Successfully copied directory from {src} to {dest}")
+    except shutil.Error as e:
+        logger.error(f"Error copying directory from {src} to {dest}: {e}")
+    except OSError as e:
+        logger.error(f"Error copying directory from {src} to {dest}: {e}")
 
-def load_executed_tasks():
-    if os.path.exists(EXECUTED_TASKS_FILE):
-        with open(EXECUTED_TASKS_FILE, 'r') as f:
-            executed_tasks = json.load(f)
-    else:
-        executed_tasks = []
-
-    return executed_tasks
-
-def execute_file(file_path):
-    subprocess.run([sys.executable, file_path])
+def zip_directory(src, dest):
+    try:
+        with zipfile.ZipFile(dest, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(src):
+                for file in files:
+                    zipf.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), src))
+        logger.info(f"Successfully zipped directory {src} to {dest}")
+    except Exception as e:
+        logger.error(f"Error zipping directory {src} to {dest}: {e}")
 
 def main():
-    config = load_config()
-    executed_tasks = load_executed_tasks()
-
-    for executed_task in executed_tasks:
-        if not executed_task.get('rerun', False):
-            execute_file(executed_task['file'])
-            executed_task['rerun'] = True
-            save_executed_tasks(executed_tasks)
-
     while True:
-        current_time = datetime.now()
+        now = datetime.now()
+        current_time_str = now.strftime("%Y-%m-%d_%H-%M-%S")
 
-        for task in config['tasks']:
-            target_day, target_time = task['timestamp'].split(' ')
-            target_hour, target_minute = target_time.split(':')
+        config = load_config()
 
-            if (current_time.day == int(target_day) and
-                current_time.hour == int(target_hour) and
-                current_time.minute == int(target_minute) and
-                not task.get('executed_today', False)):
-                execute_file(task['file'])
-                task['executed_today'] = True
-                executed_tasks.append(task)
-                save_executed_tasks(executed_tasks)
-            elif current_time.day != int(target_day):
-                task['executed_today'] = False
+        for task in config["tasks"]:
+            src = task["src"]
+            dest = task["dest"]
+            timestamp = task["timestamp"]
 
+            if current_time_str == timestamp:
+                temp_dir = os.path.join(dest, f"{current_time_str}_temp")
+                copy_directory(src, temp_dir)
+                zip_directory(temp_dir, os.path.join(dest, f"{current_time_str}.zip"))
+                shutil.rmtree(temp_dir)
+        
         time.sleep(60)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
