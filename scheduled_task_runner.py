a21a944ff56c8adf0af02081462f94727bc7a1c0
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-07 16:54:14 -0400

    update to filename

--- scheduled_task_runner.py
+++ scheduled_task_runner.py
@@ -2,11 +2,10 @@ import os
 import sys
 from datetime import datetime
 import time
-import shutil
-import json
 import logging
 from logging.handlers import RotatingFileHandler
 from task_utils import copy_directory, zip_directory, load_config
+import json
 
 # Configure logging
 logger = logging.getLogger(__name__)
@@ -16,45 +15,37 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def log_executed_task(task, timestamp):
-    executed_tasks_file = "executed_tasks.json"
-
-    if not os.path.exists(executed_tasks_file):
-        with open(executed_tasks_file, "w") as f:
+def log_execution(task_id, timestamp):
+    if not os.path.exists("executed_tasks.json"):
+        with open("executed_tasks.json", "w") as f:
             json.dump([], f)
 
-    with open(executed_tasks_file, "r") as f:
+    with open("executed_tasks.json", "r") as f:
         executed_tasks = json.load(f)
 
-    executed_task_data = {
-        "task": task,
-        "timestamp": timestamp
-    }
+    executed_tasks.append({"task_id": task_id, "timestamp": timestamp})
 
-    executed_tasks.append(executed_task_data)
-
-    with open(executed_tasks_file, "w") as f:
+    with open("executed_tasks.json", "w") as f:
         json.dump(executed_tasks, f)
 
 def main():
     config = load_config()
 
     while True:
-        current_day_of_month = datetime.now().day
-        current_time = datetime.now().strftime("%H:%M")
+        current_timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-        for task in config["tasks"]:
-            if task["day_of_month"] == current_day_of_month and task["time"] == current_time:
-                src = task["target_file"]
-                dest = task["destination"]
-                timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                temp_dir = os.path.join(dest, f"{timestamp}_temp")
+        for task_id, task in enumerate(config["tasks"]):
+            task_timestamp = f'{task["day_of_month"]} {task["time"]}'
+            if task_timestamp == current_timestamp:
+                src = task["src"]
+                dest = task["dest"]
+                temp_dir = os.path.join(dest, f"{current_timestamp}_temp")
                 copy_directory(src, temp_dir)
-                zip_directory(temp_dir, os.path.join(dest, f"{timestamp}.zip"))
+                zip_directory(temp_dir, os.path.join(dest, f"{current_timestamp}.zip"))
                 shutil.rmtree(temp_dir)
-                log_executed_task(task, timestamp)
+                log_execution(task_id, current_timestamp)
 
-        time.sleep(60)
+        time.sleep(30)
 
 if __name__ == "__main__":
     main()
