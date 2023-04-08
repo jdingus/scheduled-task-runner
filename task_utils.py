4f37299e3dd87e8660a98c19f6c7662ed122a4dd
mfgservicesgroup@gmail.com <josh.dingus@gmail.com>
2023-04-08 09:04:04 -0400

    added retry logic if copy operation fails

--- task_utils.py
+++ task_utils.py
@@ -10,14 +10,23 @@ def load_config():
     with open('config.json', 'r') as f:
         return json.load(f)
 
-def copy_directory(src, dest):
-    try:
-        shutil.copytree(src, dest)
-        logger.info(f"Successfully copied directory from {src} to {dest}")
-    except shutil.Error as e:
-        logger.error(f"Error copying directory from {src} to {dest}: {e}")
-    except OSError as e:
-        logger.error(f"Error copying directory from {src} to {dest}: {e}")
+def copy_directory(src, dest, retries=3, wait_time=60):
+    for i in range(retries):
+        try:
+            shutil.copytree(src, dest)
+            logger.info(f"Successfully copied directory from {src} to {dest}")
+            return
+        except shutil.Error as e:
+            logger.warning(f"Error copying directory from {src} to {dest}: {e}")
+        except OSError as e:
+            logger.warning(f"Error copying directory from {src} to {dest}: {e}")
+        
+        if i < retries - 1:
+            logger.info(f"Retrying copy operation in {wait_time} seconds...")
+            time.sleep(wait_time)
+
+    logger.error(f"Failed to copy directory from {src} to {dest} after {retries} attempts")
+
 
 def zip_directory(src, dest):
     try:
