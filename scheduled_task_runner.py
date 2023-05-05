a5672c737b9aa8488e226d92c48f253072ac4978
Josh Dingus <josh.dingus@gmail.com>
2023-05-05 11:02:49 -0400

    working scheduled_task_runner

--- scheduled_task_runner.py
+++ scheduled_task_runner.py
@@ -5,9 +5,9 @@ import time
 import logging
 from logging.handlers import RotatingFileHandler
 import json
+import shutil
 
 from task_utils import copy_directory, zip_directory, load_config, read_executed_tasks, save_executed_tasks, check_missed_tasks
-
 
 # Configure logging
 logger = logging.getLogger(__name__)
@@ -17,18 +17,19 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-def log_execution(task_id, timestamp):
-    if not os.path.exists("executed_tasks.json"):
-        with open("executed_tasks.json", "w") as f:
-            json.dump([], f)
+def execute_task(task):
+    src = task["src"]
+    dest = task["dest"]
+    task_name = task.get("name", "")
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    temp_dir = os.path.join(dest, f"{timestamp}_temp")
+    copy_directory(src, temp_dir)
+    zip_directory(temp_dir, os.path.join(dest, f"{timestamp} {task_name}.zip"))
+    shutil.rmtree(temp_dir)
 
-    with open("executed_tasks.json", "r") as f:
-        executed_tasks = json.load(f)
-
-    executed_tasks.append({"task_id": task_id, "timestamp": timestamp})
-
-    with open("executed_tasks.json", "w") as f:
-        json.dump(executed_tasks, f)
+    executed_tasks = read_executed_tasks()
+    executed_tasks["executed"].append({"task": task, "timestamp": timestamp})  # Fix here
+    save_executed_tasks(executed_tasks)
 
 def main():
     logger.info("Program started")
@@ -38,23 +39,18 @@ def main():
         now = datetime.now()
         current_day_of_month = now.day
         current_time = now.strftime("%H:%M")
-        print(current_time)
 
         for task in config["tasks"]:
             task_day_of_month = task["day_of_month"]
             task_time = task["time"]
-            print(task_time)
 
             if task_day_of_month == current_day_of_month and task_time == current_time:
                 task_name = task.get("name", "")
-                logger.info(f"Waiting for next task '{task_name}' at {task['time']}")
-                
-                src = task["src"]
-                dest = task["dest"]
-                task_name = task.get("name", "")
-                timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-                filename = f"{timestamp} {task_name}.zip"
+                logger.info(f"Executing task '{task_name}' at {task['time']}")
+                execute_task(task)
+                logger.info(f"Task '{task_name}' completed successfully")
+                time.sleep(60)  # Wait for 1 minute to avoid executing the same task multiple times in the same minute
         time.sleep(10)
-                
+
 if __name__ == "__main__":
     main()
